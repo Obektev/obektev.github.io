f8620e7c4e8001649c35418d2eea303c6d2b04ca
Obektev <obektev@gmail.com>
2024-10-26 12:47:15 +0300

    Tabs switching via `tab` button

--- scripts/tabSwapper.js
+++ scripts/tabSwapper.js
@@ -6,25 +6,37 @@ projects_container.hidden = true;
 document.addEventListener('DOMContentLoaded', function () {
     const select_buttons = document.querySelectorAll('.select_button');
 
+    function swapTabs(button) {
+        const button_text = button.getElementsByTagName('p')[0].textContent.toLowerCase();
+
+        if (button_text.includes('acmp - solutions'))
+            return;
+        select_buttons.forEach(btn => btn.classList.remove('selected'));
+
+        if (button_text.includes('проекты')) {
+            aboutme_container.hidden = true;
+            projects_container.hidden = false;
+        } else if (button_text.includes('обо мне')) {
+            projects_container.hidden = true;
+            aboutme_container.hidden = false;
+        }
+
+        // Add 'selected' class to the clicked button
+        this.classList.add('selected');
+    }
+
     select_buttons.forEach(button => {
         button.addEventListener('click', function () {
-
-            const button_text = button.getElementsByTagName('p')[0].textContent.toLowerCase();
-
-            if (button_text.includes('acmp - solutions'))
-                return;
-            select_buttons.forEach(btn => btn.classList.remove('selected'));
-
-            if (button_text.includes('проекты')) {
-                aboutme_container.hidden = true;
-                projects_container.hidden = false;
-            } else if (button_text.includes('обо мне')) {
-                projects_container.hidden = true;
-                aboutme_container.hidden = false;
-            }
-
-            // Add 'selected' class to the clicked button
-            this.classList.add('selected');
+            swapTabs(button);
         });
+    });
+
+    select_buttons.forEach(button => {
+       button.addEventListener('keypress', function (event) {
+           if (event.key === 'Enter' || event.key === 'Space') {
+               event.preventDefault();
+               swapTabs(button);
+           }
+       });
     });
 });
